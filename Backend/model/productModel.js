051e7adc6cb531a1fceab9e07c9548e341254b66
omotuwaojo <omotuwaojo@yahoo.com>
2026-04-29 10:01:55 +0100

    nil

--- Backend/model/productModel.js
+++ Backend/model/productModel.js
@@ -11,10 +11,10 @@ const productSchema = mongoose.Schema({
   name: {type:String, required: true, trim: true},
   description: {type:String, required: true},
   price: {type: Number, required:true},
-  discountprice: {type: Number},
+  discountPrice: {type: Number},
   image: {type: [String], required: true},
   inventory: {type:Number},
-  weight: {type: Number},
+  weight: {type: String},
   region: {type: String},
   condition: {type: String, enum: ['new', 'used', 'refurbished']},
   processor: {type: String},
@@ -31,14 +31,16 @@ const productSchema = mongoose.Schema({
   openToNegotiation: {type: Boolean, default: false},
   size: {type: String},
   color: {type:String},
+  discountPercentage: {type: Number},
   category: { type: String, enum: ['laptops', 'monitors', 'phones', 'tablets', 'accessories', 'processors'], required: true },
   productBox: {type: String},
   features: {type: String},
-  discountPercentage: {type:Number},
   rating: [ratingSchema],
   type: {type: String},
+  status: {type: String, enum: ['published', 'draft'], default: 'draft'},
   createdAt: {type:Date, default: Date.now}
 })
+
 
 const productModel = mongoose.model('product', productSchema)
 module.exports = productModel
